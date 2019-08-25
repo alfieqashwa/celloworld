ad83a09b1c1a4fbcc86bcf92e2723ad7f15e55b2
alfieqashwa <alfieqashwa@gmail.com>
2019-08-25 19:08:37 +0700

    created layout file and testing simple theme-ui layout

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,3 +1,4 @@
 module.exports = {
+  siteMetadata: {},
   plugins: [`gatsby-plugin-mdx`],
-}
+};
---
+++ src/components/layout.js
@@ -0,0 +1,14 @@
+/** @jsx jsx */
+import { jsx, Layout, Header, Main, Container, Footer } from 'theme-ui';
+
+export default ({ children }) => (
+  <Layout>
+    <Header>
+      <Container>Header</Container>
+    </Header>
+    <Main>
+      <Container>{children}</Container>
+    </Main>
+    <Footer>Celloworld @2019</Footer>
+  </Layout>
+);
