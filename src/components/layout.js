ee0b5008a1e0f0a2f92e1f6241a6b9cd8cb8df39
alfieqashwa <alfieqashwa@gmail.com>
2019-08-26 22:10:38 +0700

    starting design system  using theme-ui

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,5 +1,7 @@
 /** @jsx jsx */
 import { jsx, Layout, Main, Container, Footer } from 'theme-ui';
+import { Styled } from 'theme-ui';
+
 import Header from './header';
 
 export default ({ children }) => (
@@ -8,7 +10,9 @@ export default ({ children }) => (
       <Container>Celloworld</Container>
     </Header>
     <Main>
-      <Container>{children}</Container>
+      <Container>
+        <Styled.root>{children}</Styled.root>
+      </Container>
     </Main>
     <Footer>Celloworld @2019</Footer>
   </Layout>
